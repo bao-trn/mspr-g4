4eeff85f8bb583a106887156f0b793a250d51b94
bao <bao-trn@outlook.fr>
2023-04-18 14:28:38 +0200

    fixed some configurations

--- src/main/java/com/epsi/msprg4/MsprG4Application.java
+++ src/main/java/com/epsi/msprg4/MsprG4Application.java
@@ -2,12 +2,16 @@ package com.epsi.msprg4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MsprG4Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MsprG4Application.class, args);
+        System.out.println("hello world");
     }
 
 }
