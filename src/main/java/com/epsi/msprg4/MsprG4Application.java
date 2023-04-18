304d81f0482a443807b516d609c6444f67fc1969
bao <bao-trn@outlook.fr>
2023-04-18 14:15:06 +0200

    initial commit

---
+++ src/main/java/com/epsi/msprg4/MsprG4Application.java
@@ -0,0 +1,13 @@
+package com.epsi.msprg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsprG4Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/msprg4/MsprG4Application.java
@@ -0,0 +1,13 @@
+package com.example.msprg4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsprG4Application.class, args);
+    }
+
+}
